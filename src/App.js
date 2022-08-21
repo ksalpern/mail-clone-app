95748af9aa86f6c08cfdeaa6480e8524fa52dc9e
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-21 17:39:12 +0300

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
+      <Header/>
       <h2>Let's build gmail</h2>
     </div>
   );
