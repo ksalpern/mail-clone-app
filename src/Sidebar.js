cf476aef3901b67ac859900749a92b251469e817
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-22 15:42:09 +0300

    add sidebar footer with styles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,13 +3,16 @@ import "./Sidebar.css";
 import { Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import SidebarOption from "./SidebarOption";
-import InboxIcon from '@mui/icons-material/Inbox';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import SendIcon from '@mui/icons-material/Send';
-import NoteIcon from '@mui/icons-material/Note';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import InboxIcon from "@mui/icons-material/Inbox";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SendIcon from "@mui/icons-material/Send";
+import NoteIcon from "@mui/icons-material/Note";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { IconButton } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 function Sidebar() {
   return (
@@ -21,12 +24,31 @@ function Sidebar() {
         Compose
       </Button>
 
-      <SidebarOption Icon={InboxIcon} title='Inbox' number={54} selected={true} />
-      <SidebarOption Icon={StarBorderIcon} title='Starred' number={54} />
-      <SidebarOption Icon={AccessTimeIcon} title='Snoozed' number={54} />
-      <SidebarOption Icon={SendIcon} title='Sent' number={54} />
-      <SidebarOption Icon={NoteIcon} title='Drafts' number={54} />
-      <SidebarOption Icon={ExpandMoreIcon} title='More' number={54} />
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarBorderIcon} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={SendIcon} title="Sent" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <LocalPhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
