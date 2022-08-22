052c425b8de5a78c71c088ba37637df6c0b34b28
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-22 16:37:36 +0300

    add emailList settings icons

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,8 +1,51 @@
 import React from "react";
 import "./EmailList.css";
+import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
+import { Checkbox, IconButton } from "@mui/material";
+import RedoIcon from "@mui/icons-material/Redo";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardHideIcon from "@mui/icons-material/KeyboardHide";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function EmailList() {
-  return <div>List</div>;
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDown />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+        <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="emailList__sections">
+
+      </div>
+    </div>
+  );
 }
 
 export default EmailList;
