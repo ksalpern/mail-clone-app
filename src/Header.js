03719b0ab894cbc76aa5254b4070641195a92e46
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-22 11:09:51 +0300

    update header and it's style

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header() {
   return (
-    <div className='header'>
-      Header
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret"/>
+      </div>
+      <div className="header__right">
+      <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <AccountCircleIcon/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
