c83cc8f9c64747fca848787889e9643a2fe62ea9
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-23 15:34:06 +0300

    add icons for mail__toolsLeft

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,53 @@
 import React from "react";
 import "./Mail.css";
+import { IconButton } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import MoveToInboxIcon from "@mui/icons-material/MoveToInbox";
+import ErrorIcon from "@mui/icons-material/Error";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EmailIcon from "@mui/icons-material/Email";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Mail() {
-  return <div className="mail">
-   Mail
-  </div>;
+  return (
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton>
+            <ArrowBackIosNewIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Mail;
