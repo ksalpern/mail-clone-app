8edca56114b65722e1020d061c9eb75255d3caa0
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-09-08 21:52:20 +0300

    add onSubmit logic

--- src/SendMail.js
+++ src/SendMail.js
@@ -5,6 +5,8 @@ import { Button } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { closeSendMessage } from "./features/mailSlice";
 import { useDispatch } from "react-redux";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function SendMail() {
   const {
@@ -17,6 +19,14 @@ function SendMail() {
 
   const onSubmit = (formData) => {
     console.log(formData);
+    db.collection("emails").add({
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    dispatch(closeSendMessage());
   };
 
   return (
